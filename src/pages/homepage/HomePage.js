8be9b6697165f098c2750419bee0cc70cd8bffdb
AlexBrown4020 <balex3497@gmail.com>
2023-01-18 11:16:54 +0900

    Feat: Include company logo

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -4,8 +4,9 @@ import './homepage.css'
 
 export default function HomePage() {
     return (
-        <>
+        <section id='navbar'>
+            <img id='logo' src="https://iili.io/ZoUvBS.png" alt="Adventurers Horde Logo (circle)" border="0" />
             <h1>Adventurer's Hoard</h1>
-        </>
+        </section>
     )
 }
