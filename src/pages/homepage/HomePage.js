b251a383b910e285f15133d556eb0e8d4f7f21ff
AlexBrown4020 <balex3497@gmail.com>
2023-01-18 11:25:08 +0900

    Feat: Import and display nav buttons

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,3 +1,7 @@
+import SendIcon from '@mui/icons-material/Send';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import { Button } from '@mui/material';
 import React from 'react';
 
 import './homepage.css'
@@ -7,6 +11,17 @@ export default function HomePage() {
         <section id='navbar'>
             <img id='logo' src="https://iili.io/ZoUvBS.png" alt="Adventurers Horde Logo (circle)" border="0" />
             <h1>Adventurer's Hoard</h1>
+            <div id='navbuttons'>
+                <Button variant='outlined' startIcon={<SendIcon/>}>
+                    Top
+                </Button>
+                <Button variant='outlined' startIcon={<CameraAltIcon/>}>
+                    Images
+                </Button>
+                <Button variant='outlined' startIcon={<ContactPhoneIcon/>}>
+                    Contact
+                </Button>
+            </div>
         </section>
     )
 }
