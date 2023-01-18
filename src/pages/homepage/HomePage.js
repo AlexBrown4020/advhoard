567d24fff7505e1dbd7e8031b3dcd6b136b69cf0
AlexBrown4020 <balex3497@gmail.com>
2023-01-18 11:08:01 +0900

    Feat: Implement beginning web page

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './homepage.css'
+
+export default function HomePage() {
+    return (
+        <>
+            <h1>Adventurer's Hoard</h1>
+        </>
+    )
+}
