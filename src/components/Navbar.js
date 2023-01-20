deb3fbf4d51028bbc042e29faf46e1a0e6b448c2
AlexBrown4020 <balex3497@gmail.com>
2023-01-20 11:20:44 +0900

    Feat: Implement navbar changes and stylig

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,15 @@ import './navbar.css';
 export const Navbar = () => {
     return (
         <div id='nav'>
-            <Button variant='outlined' startIcon={<SendIcon/>}>
+            <img id='logo' src="https://iili.io/ZoUvBS.png" alt="Adventurers Hoard Logo (circle)" border="0" />
+            <h1 id='title'>Adventurer's Hoard</h1>
+            <Button sx={{color: 'white', border: '2px white solid', height: 30}} variant='outlined' startIcon={<SendIcon/>}>
                 Top
             </Button>
-            <Button variant='outlined' startIcon={<CameraAltIcon/>}>
+            <Button sx={{color: 'white', border: '2px white solid', height: 30}} variant='outlined' startIcon={<CameraAltIcon/>}>
                 Images
             </Button>
-            <Button variant='outlined' startIcon={<ContactPhoneIcon/>}>
+            <Button sx={{color: 'white', border: '2px white solid', height: 30}} variant='outlined' startIcon={<ContactPhoneIcon/>}>
                 Contact
             </Button>
         </div>
--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -7,12 +7,9 @@ export default function HomePage() {
     return (
         <div id='home'>
             <Navbar/>
-            <section id='title'>
-                <img id='logo' src="https://iili.io/ZoUvBS.png" alt="Adventurers Hoard Logo (circle)" border="0" />
-                <h1>Adventurer's Hoard</h1>
-            </section>
             <section id='images'>
                 <img class='table-image' alt='oni mask' src='https://iili.io/ynZ4mF.jpg'/>
+                <img class='table-image' alt='painted oni' src='https://iili.io/ynZv0Q.jpg'/>
                 <img class='table-image' alt='magic ring' src='https://iili.io/ynZ2UB.jpg'/>
                 <img class='table-image' alt='puzzle box' src='https://iili.io/ynilat.jpg'/>
                 <img class='table-image' alt='flintlock' src='https://iili.io/HaQy5qx.png'/>
@@ -26,7 +23,6 @@ export default function HomePage() {
                 <img class='table-image' alt='gorgon' src='https://iili.io/HaQm5hb.jpg'/>
                 <img class='table-image' alt='gnoll' src='https://iili.io/HaQmo42.jpg'/>
                 <img class='table-image' alt='hydra' src='https://iili.io/HaQmKIs.jpg'/>
-                <img class='table-image' alt='painted oni' src='https://iili.io/ynZv0Q.jpg'/>
                 <img class='table-image' alt='holy symbol' src='https://iili.io/ynZ7bn.jpg'/>
             </section>
 
