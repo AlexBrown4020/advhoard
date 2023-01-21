a3395394ac6cf964250b037e309893ef7a5bd015
AlexBrown4020 <balex3497@gmail.com>
2023-01-21 12:51:22 +0900

    Feat: Make mock image data, loop through and render

--- src/components/images/Images.js
+++ src/components/images/Images.js
@@ -1,24 +1,32 @@
 import './images.css';
 
 export function Images() {
+
+    const imageData = [{src:'https://iili.io/ynZ4mF.jpg'},
+    {src:'https://iili.io/ynZv0Q.jpg'},
+    {src:'https://iili.io/ynZ2UB.jpg'},
+    {src:'https://iili.io/ynilat.jpg'},
+    {src:'https://iili.io/HaQy5qx.png'},
+    {src:'https://iili.io/HaQyHOX.png'},
+    {src:'https://iili.io/HaQp4HP.png'},
+    {src:'https://iili.io/HaQpVl2.png'},
+    {src:'https://iili.io/HaQpBAF.png'},
+    {src:'https://iili.io/HaQms6l.png'},
+    {src:'https://iili.io/HaQm4ZG.png'},
+    {src:'https://iili.io/HaQmNGR.jpg'},
+    {src:'https://iili.io/HaQm5hb.jpg'},
+    {src:'https://iili.io/HaQmo42.jpg'},
+    {src:'https://iili.io/HaQmKIs.jpg'},
+    {src:'https://iili.io/ynZ7bn.jpg'},
+]
+
     return (
         <section id='images'>
-            <img class='table-image' alt='oni mask' src='https://iili.io/ynZ4mF.jpg'/>
-            <img class='table-image' alt='painted oni' src='https://iili.io/ynZv0Q.jpg'/>
-            <img class='table-image' alt='magic ring' src='https://iili.io/ynZ2UB.jpg'/>
-            <img class='table-image' alt='puzzle box' src='https://iili.io/ynilat.jpg'/>
-            <img class='table-image' alt='flintlock' src='https://iili.io/HaQy5qx.png'/>
-            <img class='table-image' alt='bows' src='https://iili.io/HaQyHOX.png'/>
-            <img class='table-image' alt='revolver' src='https://iili.io/HaQp4HP.png'/>
-            <img class='table-image' alt='rifle' src='https://iili.io/HaQpVl2.png'/>
-            <img class='table-image' alt='tower creatures' src='https://iili.io/HaQpBAF.png'/>
-            <img class='table-image' alt='giant' src='https://iili.io/HaQms6l.png'/>
-            <img class='table-image' alt='naga' src='https://iili.io/HaQm4ZG.png'/>
-            <img class='table-image' alt='wyvern' src='https://iili.io/HaQmNGR.jpg'/>
-            <img class='table-image' alt='gorgon' src='https://iili.io/HaQm5hb.jpg'/>
-            <img class='table-image' alt='gnoll' src='https://iili.io/HaQmo42.jpg'/>
-            <img class='table-image' alt='hydra' src='https://iili.io/HaQmKIs.jpg'/>
-            <img class='table-image' alt='holy symbol' src='https://iili.io/ynZ7bn.jpg'/>
-    </section>
+            {
+                imageData.map((obj) => {
+                    return <img className='table-image' alt='' src={obj.src}></img>
+                })
+            }
+        </section>
     )
 }
