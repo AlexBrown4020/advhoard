43d9ed1d4289abe2505385fc49686803175c9bbb
Alex Brown <balex3497@gmail.com>
2023-01-22 11:46:48 +0900

    Feat: Prevent screen from widening on small screens, simplify nav button styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import SendIcon from '@mui/icons-material/Send';
 import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import { Button } from '@mui/material';
+import { IconButton } from '@mui/material';
 
 import './navbar.css';
 
@@ -10,15 +10,15 @@ export const Navbar = () => {
         <div id='nav'>
             <img id='logo' src="https://iili.io/ZoUvBS.png" alt="Adventurers Hoard Logo (circle)" border="0" />
             <h1 id='title'>Adventurer's Hoard</h1>
-            <Button sx={{color: 'white', border: '2px white solid', height: 30}} variant='outlined' startIcon={<SendIcon/>}>
-                Top
-            </Button>
-            <Button sx={{color: 'white', border: '2px white solid', height: 30}} variant='outlined' startIcon={<CameraAltIcon/>}>
-                Images
-            </Button>
-            <Button sx={{color: 'white', border: '2px white solid', height: 30}} variant='outlined' startIcon={<ContactPhoneIcon/>}>
-                Contact
-            </Button>
+            <IconButton sx={{color: 'white', height: 30}} variant='outlined'>
+                <SendIcon/>
+            </IconButton>
+            <IconButton sx={{color: 'white', height: 30}} variant='outlined'>
+                <CameraAltIcon/>
+            </IconButton>
+            <IconButton sx={{color: 'white', height: 30}} variant='outlined'>
+                <ContactPhoneIcon/>
+            </IconButton>
         </div>
     )
 
