33c983ef8c85606803829d464da0cf32b34f36d1
Alex Brown <balex3497@gmail.com>
2023-01-22 12:17:10 +0900

    Feat: Adjust nav items from redirect banner to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,24 @@
+import { IconButton } from '@mui/material'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import HomeIcon from '@mui/icons-material/Home';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 import './footer.css';
 
 export const Footer = () => {
     return (
-        <section>
-            <h1>Hi</h1>
+        <section id='footer'>
+            <p>Alexander Brown, all rights reserved</p>
+            <p>Updated: 22.Jan.2023</p>
+            <IconButton>
+                <HomeIcon/>
+            </IconButton>
+            <IconButton>
+                <LinkedInIcon/>
+            </IconButton>
+            <IconButton>
+                <GitHubIcon/>
+            </IconButton>
         </section>
     )
 }
