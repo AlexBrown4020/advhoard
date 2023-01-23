437628cc3fe9b84305b75343f46255282022a71f
AlexBrown4020 <balex3497@gmail.com>
2023-01-23 17:16:24 +0900

    Feat: reorganize redirect information

--- src/components/redirect/Redirect.js
+++ src/components/redirect/Redirect.js
@@ -4,8 +4,9 @@ export const Redirect = () => {
     return (
         <div id='redirect'>
             <h2 id='top-banner'>100's of Designs</h2>
-            <h3 id='mid-banner'>Blender, Sculptris, Character/Object Design</h3>
-            <h3 id='mid-banner'>FDM/SLA Printing</h3>
+            <h3 id='mid-banner'>- Blender, Sculptris</h3>
+            <h3 id='mid-banner'>- Character/Object Design</h3>
+            <h3 id='mid-banner'>- FDM/SLA Printing</h3>
         </div>
     )
 
