ed0c8ad8e1a6716fe8f03d9325711a0cfc2091cd
AlexBrown4020 <balex3497@gmail.com>
2023-01-25 10:49:17 +0900

    Feat: Relocate suggestions to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,6 @@
-import { IconButton } from '@mui/material'
+import { IconButton } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat';
+import EngineeringIcon from '@mui/icons-material/Engineering';
 import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import HomeIcon from '@mui/icons-material/Home';
@@ -9,36 +11,53 @@ import './footer.css';
 export const Footer = () => {
     return (
         <section id='footer'>
-            <div className='footerTextContainer'>
-                <p className='footer-text'>Alexander Brown, All rights reserved</p>
-            </div>
-            <div id='website-container'>
-                <div className='website-link' >
-                    <IconButton href='https://portfolio-page-adb.herokuapp.com/index.html'>
-                        <HomeIcon sx={{color:'white'}}/>
+            <div id='suggestion-container'>
+                <div className='suggestion-icon-container'>
+                    <IconButton sx={{'justify-content': 'center', display: 'flex', 'flex-direction': 'column', color:'white'}}>
+                        <p className='suggestion-title'>Suggestions</p>
+                        <ChatIcon/>
                     </IconButton>
                 </div>
-                <div className='website-link'>
-                    <IconButton href='https://www.linkedin.com/in/alex-brown-2a6b7820a/'>
-                        <LinkedInIcon sx={{color:'white'}}/>
-                    </IconButton>
-                </div>
-                <div className='website-link'>
-                    <IconButton href='https://github.com/AlexBrown4020'>
-                        <GitHubIcon sx={{color:'white'}}/>
-                    </IconButton>
-                </div>
-                <div className='website-link' style={{'border-right': 'white 2px solid'}}>
-                    <IconButton href='https://www.etsy.com/uk/shop/AdventurersHoardShop?ref=seller-platform-mcnav'>
-                        <LocalGroceryStoreIcon sx={{color:'white'}}/>
+                <div className='suggestion-icon-container'>
+                    <IconButton sx={{'justify-content': 'center', display: 'flex', 'flex-direction': 'column', color:'white'}}>
+                        <p className='suggestion-title'>Requests</p>
+                        <EngineeringIcon/>
                     </IconButton>
                 </div>
             </div>
-            <div className='footerTextContainer'>
-                <p className='footer-text'>
-                © 25th January 2023
-                </p>
+            <div id='info-container'>
+                <div className='footerTextContainer'>
+                    <p className='footer-text'>Alexander Brown, All rights reserved</p>
+                </div>
+                <div id='website-container'>
+                    <div className='website-link' >
+                        <IconButton href='https://portfolio-page-adb.herokuapp.com/index.html'>
+                            <HomeIcon sx={{color:'white'}}/>
+                        </IconButton>
+                    </div>
+                    <div className='website-link'>
+                        <IconButton href='https://www.linkedin.com/in/alex-brown-2a6b7820a/'>
+                            <LinkedInIcon sx={{color:'white'}}/>
+                        </IconButton>
+                    </div>
+                    <div className='website-link'>
+                        <IconButton href='https://github.com/AlexBrown4020'>
+                            <GitHubIcon sx={{color:'white'}}/>
+                        </IconButton>
+                    </div>
+                    <div className='website-link' style={{'border-right': 'white 2px solid'}}>
+                        <IconButton href='https://www.etsy.com/uk/shop/AdventurersHoardShop?ref=seller-platform-mcnav'>
+                            <LocalGroceryStoreIcon sx={{color:'white'}}/>
+                        </IconButton>
+                    </div>
+                </div>
+                <div className='footerTextContainer'>
+                    <p className='footer-text'>
+                    © 25th January 2023
+                    </p>
+                </div>
             </div>
+           
         </section>
     )
 }
