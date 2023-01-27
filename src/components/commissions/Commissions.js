58ccdd307abdb474375aa5f59c68198684a7807b
AlexBrown4020 <balex3497@gmail.com>
2023-01-27 13:00:01 +0900

    Feat: Commission information

--- src/components/commissions/Commissions.js
+++ src/components/commissions/Commissions.js
@@ -12,6 +12,18 @@ export const Commissions = ({toggleC}) => {
                     <CloseIcon/>
                 </IconButton>
             </div>
+            <div>
+                <p className='contact-info'>When requesting a commission, please note:</p>
+                <ul>
+                    <li className='contact-info'>Depending on current work load, commissions may take time.</li>
+                    <li className='contact-info'>Designing custom 3D files will be charged by hour.</li>
+                    <li className='contact-info'>If you require a manufactured model, there is a charge for materials and time spent. Designing new 3D files is also included in this fee.</li>
+                </ul>
+                <p className='contact-info'>
+                    Within your email; please include any information to help with the design process 
+                    (images, videos, schematics, etc) to get the closest approximation of your product.  
+                </p>
+            </div>
             <form id='contact-bottom'>
                 <TextField sx={{width: '50%', padding: '5px', label:{color: 'white'}, input:{color: 'white'}}} variant='filled' label='Name' type='text' name='from_name'/>
                 <TextField sx={{width: '50%', padding: '5px', label:{color: 'white'}, input:{color: 'white'}}} variant='filled' label='Email' type='text'  name='reply_to'/>
