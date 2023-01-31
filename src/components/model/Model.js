cd1f521ed7f414c5143940e283c7ca8bd6a7f111
AlexBrown4020 <balex3497@gmail.com>
2023-01-31 11:27:42 +0900

    Fix: Resolve camera focus issue, repositioned object in Blender to 0-origin

--- src/components/model/Model.js
+++ src/components/model/Model.js
@@ -4,17 +4,26 @@ import { OrbitControls } from "@react-three/drei";
 import { LC } from "../LC";
 
 export const Model = () => {
+
+    const Box = () => {
+        return (
+          <mesh>
+            <boxGeometry attach='geometry'/>
+          </mesh>
+        );
+      }
+
     return (
-        <Canvas style={{
-            height: '100vh',
-        }}>
-            <LC/>
-            <OrbitControls/>
+        <Canvas style={{height: '100vh',width: '100vw'}}>
+            <OrbitControls />
             <ambientLight intensity={0.5}/>
+            <pointLight position={[10,5,10]}/>
             <spotLight
                 position={[10, 15, 10]}
                 angle={0.3}
             />
+            <Box/>
+            <LC/>
         </Canvas>
     )
 }
