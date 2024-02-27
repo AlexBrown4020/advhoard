580028fb7f544b18039420ce5da7dd45ddb2da9d
AlexBrown4020 <balex3497@gmail.com>
2024-02-27 03:10:47 +0000

    Feat: Add pages and popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import { Navbar } from "./components/navbar/Navbar";
+import { Home, About, Projects, Work, Contact } from './pages';
 
 const App= () => {
     return (
@@ -8,11 +9,11 @@ const App= () => {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={"Home"} />
-                    <Route path="/about" element={"About"} />
-                    <Route path="/projects" element={"Projects"} />
-                    <Route path="/work" element={"Work"} />
-                    <Route path="/contact" element={"Contact"} />
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/about" element={<About/>} />
+                    <Route path="/projects" element={<Projects/>} />
+                    <Route path="/work" element={<Work/>} />
+                    <Route path="/contact" element={<Contact/>} />
                 </Routes>
             </Router>
         </main>
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Work from './Work';
+import Contact from './Contact';
+
+export { Home, About, Projects, Work, Contact};
