23a1fe440ce5bbf6764e926e169352c89c661e86
AlexBrown4020 <balex3497@gmail.com>
2024-02-27 03:48:42 +0000

    Feat: Implement loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { React, Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/navbar/Loader'
 
 const Home = () => {
   return (
     <section className='w-full h-screen relative'>
-        <div className='absolute top-28 left-0 right-0 z-10 items-center justify-center'>
+        {/* <div className='absolute top-28 left-0 right-0 z-10 items-center justify-center'>
             POPUP
-        </div>
+        </div> */}
+        <Canvas className="w-full h-screen bg-transparent"
+            camera={{near: 0.1, far: 1000}}
+        >
+            <Suspense fallback={<Loader />} >
+                <directionalLight />
+                <ambientLight />
+                
+            </Suspense>
+        </Canvas>
     </section>
   )
 }
